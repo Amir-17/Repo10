ebfc88c5421b560230bb50d10cd6ce203c0d3d13
Amir-17 <amirkahriman17@gmail.com>
2022-04-07 10:34:05 +0200

    Fixing again...

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,11 +1,11 @@
-function myfunction(roar) {
+function myfunction(propertyName) {
     var lion = {
         "name": "Simba",
         "legs": 4,
         "tails": 1,
         "roar": "roar-roar"
     };
-    delete lion[roar];
+    delete lion[propertyName];
     return lion;
 };
 console.log(myfunction("roar"));
